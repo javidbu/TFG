3ae4e451b161540e05d5ddb4e598b1aca4b8394e
Javi <javidbu@gmail.com>
2015-04-30 18:39:51 +0200

    Arreglados bugs en leer.py
    Ya cumple todas sus funciones. La tarea completa de leer los datos
    de Split_1.txt, guardarlos en diccionarios, guardar los dicciona-
    rios en archivos con formato pickle, leer los archivos pickle en
    diccionarios nuevos y comparar ambos diccionarios para verificar
    que todo ha ido bien ha tardado un poco...

--- leer.py
+++ leer.py
@@ -1,10 +1,11 @@
+# -*- coding: cp1252 -*-
 import pickle as pk
 '''Esto deberia servir para leer los datos de los .txt y guardarlos
    en diccionarios. Tambien para guardarlos con pickle y volver a
-   abrirlos. De todas formas, con los datos de las transacciones
-   mi ordenador peta al intentar guardarlos (Memory Error).
-   Ha conseguido guardar los datos de Split_1.txt en diccionarios,
-   pero guardarlos con pickle ha sido demasiado para el pobre...'''
+   abrirlos. Ha funcionado (tarda un poco...) con los datos del ar-
+   chivo Split_1.txt.'''
+
+i = 0
 
 def leer(archivo = 'X.txt'):
     '''Lee los datos almacenados en ARCHIVO, que tienen que ser filas
@@ -13,13 +14,18 @@ def leer(archivo = 'X.txt'):
        como clave para los diccionarios la primera palabra separada
        por "|" de la fila, y como valor la ultima palabra para DICY y
        una lista con el resto de palabras de la fila para DICX'''
+    #Los campos 0, 3, 14, 16 y 41 no tenian numeros en todas las entradas de Split_1.txt.
     dicX = {}
     dicY = {}
+    global i
     try:
         f = open(archivo,'r')
         for line in f:
-            lista = line.split('|')
-            dicX[lista[0]] = lista[1:]
+            lista = line.split('|')[:-1] #Al final hay un '|' que hay que quitar
+            #El primer campo, que estaba usando como clave del diccionario, da problemas. Pongo otra clave distinta (unica para cada transaccion, en teoria)
+            dicX[i] = floating(lista[1:3] + lista[4:14] + lista[15:16] + lista[17:41] + lista[42:-1]) #el campo 14 daba problemas...
+            dicY[i] = int(lista[-1])
+            i += 1
         f.close()
         return dicX,dicY
     except:
@@ -47,12 +53,23 @@ def abrir(archivo = 'Xnew.txt'):
         print 'Error al leer el archivo',archivo
         return None
 
-dicX,dicY = leer()
-guardar(dicX)
-guardar(dicY,'Ynew.txt')
-Xnew = abrir()
-Ynew = abrir('Ynew.txt')
-print dicX == Xnew
-print dicY == Ynew
+def floating(lista):
+    '''Recibe una lista de strings y devuelve una lista de enteros'''
+    #Lo dejo con int o lo cambio a float??
+    l = []
+    global numero
+    numero = 0
+    for elemento in lista:
+        l += [int(elemento)]
+        numero += 1
+    return l
+
+##dicX,dicY = leer('training/Split_1.txt')
+##guardar(dicX,'X_1.txt')
+##guardar(dicY,'Y_1.txt')
+##Xnew = abrir('X_1.txt')
+##Ynew = abrir('Y_1.txt')
+##print dicX == Xnew
+##print dicY == Ynew
 
 
