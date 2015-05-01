63e592f5d6f54e4b44b596cd3510580712f9d5fa
Javi <javidbu@gmail.com>
2015-05-01 19:11:38 +0200

    Tiempos medidos en logReg.py
    La funcion es bastante lenta, el problema es que lo hace todo con
    bucles, y son muchos calculos. Lo que mas ha tardado en mi portatil
    ha sido el gradiente, 33 minutos!! Leer los datos de X tarda 7 min
    y medio, y calcular el coste 2 minutos. En leer los datos de y solo
    tarda 18 segundos...
    
    Next step: Implementarlo todo con vectores y matrices! (Otra vez)

--- logReg.py
+++ logReg.py
@@ -2,6 +2,7 @@
 import numpy as np
 from scipy.optimize import minimize
 from leer import *
+import time
 #minimize da errores!!!
 
 ##def main(X,y,Lambda = 0,comprob = False):
@@ -58,7 +59,17 @@ def sigmoid(z):
 
 ##Regresion lineal con clases y diccionarios:
 class LogReg:
+    '''Implementa una regresión logistica. Para llamarla, ejecu-
+       tar LogReg(X,y,Lambda,umbral), donde X es un diccionario
+       con listas de los valores de las variables (features), y
+       es un diccionario con valores 0 o 1 y las mismas claves
+       que X, Lambda es el parametro de regularizacion (para evi-
+       tar el overfitting) y umbral es el umbral a partir del
+       cual vamos a predecir y = 1.'''
     def __init__(self,X,y,Lambda = 0, umbral = 0.5):
+        '''Inicia la regresion logistica, calcula el valor optimo
+           de theta e imprime en pantalla algunos datos sobre la
+           bondad de la prediccion.'''
         #Añadir Xval, Xtest, yval, ytest?
         self.X = X
         self.y = y
@@ -74,52 +85,77 @@ class LogReg:
 ##        self.nOutput = len(y.values()[0]) #Numero de clases de salida. En el problema binario sera 1
 ##        if self.nOutput != 1:
 ##            raise TypeError('De momento no estoy implementado para resolver clasificaciones multiclase')
+        print 'Variables de la clase creadas'
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
         self.init_theta = [0]*(self.nFeat+1) #theta inicial, todo ceros
+        print 'Theta inicial a cero'
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
         self.comparar(self.y,self.predict(self.X))
     def h(self,theta,x): #Hipotesis para valores dados de theta y x
+        '''Calcula la h_theta(x) de la regresion logistica.'''
         suma = 0
-        for i in range(len(theta)):
+        for i in range(len(theta)): #Esto estaría mejor con vectores...
             suma += theta[i]*x[i] 
         return sigmoid(suma)         
-    def coste(self, theta, X, y, Lambda):
+    def coste(self, theta, X, y, Lambda): #Tarda 2 minutos... El problema serio esta en el gradiente, pero esto tampoco es admisible...
+        '''Calcula el valor de la funcion de coste para valores dados
+           de theta, X, y y Lambda'''
+        print 'Llamada a la funcion de coste'
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
         suma = 0.
         keys = X.keys()
         m = len(keys)
-        for k in xrange(m):
-            suma += y[keys[k]]*np.log(self.h(theta,X[keys[k]]))
+        for k in xrange(m): #Esto iria mejor con vectores
+            suma += y[keys[k]]*np.log(self.h(theta,X[keys[k]]))#Aqui da errores, igual seria mejor que calculase las cosas con un if (ademas supongo que sera mas rapido)
             suma += (1-y[keys[k]])*np.log(1-self.h(theta,X[keys[k]]))
         suma = suma/(float(m))
         suma2 = 0.
-        for j in range(1,len(theta)):
+        for j in range(1,len(theta)): #Regularizacion, ignoramos theta_0
             suma2 += theta[j]**2
         suma2 = suma2*Lambda/(2.*m)
+        print 'Coste calculado'
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
         return -suma + suma2
-    def grad(self, theta, X, y, Lambda):
+    def grad(self, theta, X, y, Lambda):#Esto parece ser lo que mas tarda... 33 minutos!
+        print 'Llamada al gradiente'
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
         keys = X.keys()
         m = len(keys)
         grad = [0]*len(theta)
         for j in range(len(grad)):
-            for k in xrange(m):
+            for k in xrange(m): #Hay que usar vectores y matrices!!!
                 grad[j] += (self.h(theta,X[keys[k]])-y[keys[k]])*X[keys[k]][j]
             if j != 0:
                 grad[j] += Lambda*theta[j]
+        print 'Gradiente calculado'
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
         return [x/float(m) for x in grad]
     def optim(self):
+        print 'Optimizando theta'
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
         res = minimize(self.coste, self.init_theta,
                        args = (self.X, self.y, self.Lambda),
                        method = 'BFGS', jac=self.grad,
                        options={'maxiter': 400,'disp':True})
         self.theta = res.x
+        print 'Theta optimizada'
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
     def predict(self,X):
         self.optim()
         keys = X.keys()
         m = len(keys)
         ypred = {}
+        print 'Empezando la prediccion'
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
         for i in xrange(m):
             if self.h(self.theta,X[keys[i]]) >= self.umbral: ypred[keys[i]] = 1
             else: ypred[keys[i]] = 0
+        print 'Prediccion realizada'
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
         return ypred
     def comparar(self,y,ypred):
+        print 'Empezando la comparacion'
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
         keys = y.keys()
         m = len(keys)
         mpred = len(ypred.keys())
@@ -152,9 +188,17 @@ class LogReg:
         print 'Precission:',prec,'%\n'
         print 'F score:',f,'%\n'
         print 'G mean:',g,'%\n'
-        
-X = abrir('X_1.txt')
-y = abrir('y_1.txt')
+        print 'Hora actual: ' + time.strftime("%H:%M:%S")
+
+print 'Leyendo los datos...'
+print 'Hora actual: ' + time.strftime("%H:%M:%S")
+X = abrir('X_1.txt') #Tarda 7 minutos y medio... ¿No se puede mejorar de ninguna forma?
+print 'Leido archivo X'
+print 'Hora actual: ' + time.strftime("%H:%M:%S")
+y = abrir('y_1.txt') #Tarda 18 segundos, por fin algo normal!
+print 'Leido archivo y'
+print 'Instanciando la clase'
+print 'Hora actual: ' + time.strftime("%H:%M:%S")
 prob = LogReg(X,y,Lambda=0,umbral=0.5)
             
         
